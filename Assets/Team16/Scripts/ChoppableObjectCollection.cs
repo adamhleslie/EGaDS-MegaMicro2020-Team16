70bf1714ed15a4953037c9ac899d3a9ada6a7574
Adam Leslie <adamhleslie@gmail.com>
2020-12-27 16:00:42 -0600

    Adds ChoppableObjectCollection

---
+++ Assets/Team16/Scripts/ChoppableObjectCollection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ChoppableObjectCollection", menuName = "Choppable Object Collection")]
+public class ChoppableObjectCollection : ScriptableObject
+{
+	[SerializeField]
+	private ChoppableObject[] _choppableObjects;
+
+	// TODO: Need to make this randomized!
+	public ChoppableObject[] GetRandomUnique()
+	{
+		return new ChoppableObject[] { _choppableObjects[0] };
+	}
+}
---
+++ Assets/Team16/Scripts/ChoppingMinigameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team16
+{
+    public class ChoppingMinigameManager : MonoBehaviour
+    {
+        [SerializeField]
+        private ChoppableObjectCollection _choppableObjects;
+    }
+}
+
