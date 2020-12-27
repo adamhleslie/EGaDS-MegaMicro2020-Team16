03602c763f57ffca72214965c1f2cefa93d41459
Adam Leslie <adamhleslie@gmail.com>
2020-12-27 16:25:56 -0600

    Adds usage of randome choppable objects

--- Assets/Team16/Scripts/ChoppingMinigameManager.cs
+++ Assets/Team16/Scripts/ChoppingMinigameManager.cs
@@ -6,8 +6,34 @@ namespace Team16
 {
     public class ChoppingMinigameManager : MonoBehaviour
     {
-        [SerializeField]
-        private ChoppableObjectCollection _choppableObjects;
-    }
-}
+		[SerializeField]
+		private ChoppableObjectCollection _choppableObjects;
+		[SerializeField]
+		private float _timerLength;
 
+		private ChoppableObject[] _usedObjects;
+
+		void Start()
+		{
+			_usedObjects = _choppableObjects.GetRandomUnique();
+			if ((_usedObjects == null) || (_usedObjects.Length == 0))
+			{
+				Debug.LogError("[ChoppingMinigameManager.Start] No choppable objects!");
+				return;
+			}
+
+			foreach (ChoppableObject choppableObject in _usedObjects)
+			{
+				Debug.Log($"Choppable object: {choppableObject.name}");
+			}
+
+			StartCoroutine(TimerCoroutine());
+		}
+
+		private IEnumerator TimerCoroutine()
+		{
+			yield return new WaitForSeconds(_timerLength);
+			Debug.Log($"{_timerLength}s have passed");
+		}
+	}
+}
